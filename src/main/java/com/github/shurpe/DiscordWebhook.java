db1d6cec536285019418d72128755ab3eb2c7ac7
ps999player <167452886+ps999player@users.noreply.github.com>
2025-12-22 10:37:31 -0500

    Update DiscordWebhook.java

--- src/main/java/com/github/shurpe/DiscordWebhook.java
+++ src/main/java/com/github/shurpe/DiscordWebhook.java
@@ -2,194 +2,120 @@ package com.github.shurpe;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class a {
 
-    private final String b;
-    private String c, d, e;
-    private boolean f;
-    private final List<g> g = new ArrayList<>();
+    private final String a;
+    private String b, c, d;
+    private boolean e;
+    private final List<b> f = new ArrayList<>();
 
-    public a(String b) {
-        this.b = b;
+    public a(String a) {
+        this.a = a;
     }
 
-    public a a(String c) {
+    public a a(String b) {
+        this.b = b;
+        return this;
+    }
+
+    public a b(String c) {
         this.c = c;
         return this;
     }
 
-    public a b(String d) {
+    public a c(String d) {
         this.d = d;
         return this;
     }
 
-    public a c(String e) {
+    public a a(boolean e) {
         this.e = e;
         return this;
     }
 
-    public a d(boolean f) {
-        this.f = f;
+    public a a(b g) {
+        this.f.add(g);
         return this;
     }
 
-    public a a(g h) {
-        this.g.add(h);
-        return this;
-    }
+    public void a() throws IOException, InterruptedException {
+        JsonObject g = new JsonObject();
+        g.addProperty("content", b);
+        g.addProperty("username", c);
+        g.addProperty("avatar_url", d);
+        g.addProperty("tts", e);
 
-    public void e() throws IOException {
-        JsonObject i = new JsonObject();
-
-        i.addProperty("content", this.c);
-        i.addProperty("username", this.d);
-        i.addProperty("avatar_url", this.e);
-        i.addProperty("tts", this.f);
-
-        if (!this.g.isEmpty()) {
-            JsonArray j = new JsonArray();
-
-            for (g k : this.g) {
-                JsonObject l = new JsonObject();
-
-                l.addProperty("title", k.a());
-                l.addProperty("description", k.b());
-                l.addProperty("url", k.c());
-
-                if (k.d() != -1)
-                    l.addProperty("color", k.d());
-
-                g.m n = k.e();
-                g.o p = k.f();
-                g.q r = k.g();
-                g.s t = k.h();
-                List<g.u> v = k.i();
-
-                if (n != null) {
-                    JsonObject w = new JsonObject();
-                    w.addProperty("text", n.a());
-                    w.addProperty("icon_url", n.b());
-                    l.add("footer", w);
-                }
-
-                if (p != null) {
-                    JsonObject x = new JsonObject();
-                    x.addProperty("url", p.a());
-                    l.add("image", x);
-                }
-
-                if (r != null) {
-                    JsonObject y = new JsonObject();
-                    y.addProperty("url", r.a());
-                    l.add("thumbnail", y);
-                }
-
-                if (t != null) {
-                    JsonObject z = new JsonObject();
-                    z.addProperty("name", t.a());
-                    z.addProperty("url", t.b());
-                    z.addProperty("icon_url", t.c());
-                    l.add("author", z);
-                }
-
-                JsonArray aa = new JsonArray();
-                for (g.u ab : v) {
-                    JsonObject ac = new JsonObject();
-                    ac.addProperty("name", ab.a());
-                    ac.addProperty("value", ab.b());
-                    ac.addProperty("inline", ab.c());
-                    aa.add(ac);
-                }
-                l.add("fields", aa);
-
-                j.add(l);
+        if (!f.isEmpty()) {
+            JsonArray h = new JsonArray();
+            for (b i : f) {
+                h.add(i.a());
             }
-
-            i.add("embeds", j);
+            g.add("embeds", h);
         }
 
-        try (CloseableHttpClient ad = HttpClients.createDefault()) {
-            HttpPost ae = new HttpPost(this.b);
-            ae.setHeader("Accept", "application/json");
-            ae.setHeader("Content-type", "application/json");
-            ae.setEntity(new StringEntity(i.toString()));
+        HttpRequest j = HttpRequest.newBuilder()
+                .uri(URI.create(a))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(g.toString()))
+                .build();
 
-            ad.execute(ae);
-        }
+        HttpClient.newHttpClient()
+                .send(j, HttpResponse.BodyHandlers.discarding());
     }
 
-    public static class g {
+    // ============== EMBED ==============
 
+    public static final class b {
         private String a, b, c;
         private int d = -1;
-        private m e;
-        private q f;
-        private o g;
-        private s h;
-        private final List<u> i = new ArrayList<>();
+        private final List<c> e = new ArrayList<>();
 
-        public String a() { return a; }
-        public String b() { return b; }
-        public String c() { return c; }
-        public int d() { return d; }
-        public m e() { return e; }
-        public q f() { return f; }
-        public o g() { return g; }
-        public s h() { return h; }
-        public List<u> i() { return i; }
+        public b a(String a) { this.a = a; return this; }
+        public b b(String b) { this.b = b; return this; }
+        public b c(String c) { this.c = c; return this; }
+        public b a(int d) { this.d = d; return this; }
 
-        public g a(String a) { this.a = a; return this; }
-        public g b(String b) { this.b = b; return this; }
-        public g c(String c) { this.c = c; return this; }
-        public g d(int d) { this.d = d; return this; }
-        public g e(String text, String icon) { this.e = new m(text, icon); return this; }
-        public g f(String url) { this.f = new q(url); return this; }
-        public g g(String url) { this.g = new o(url); return this; }
-        public g h(String name, String url, String icon) { this.h = new s(name, url, icon); return this; }
-        public g i(String name, String value, boolean inline) { this.i.add(new u(name, value, inline)); return this; }
-
-        private static class m {
-            private final String a, b;
-            private m(String a, String b) { this.a = a; this.b = b; }
-            private String a() { return a; }
-            private String b() { return b; }
+        public b a(String a, String b, boolean c) {
+            this.e.add(new c(a, b, c));
+            return this;
         }
 
-        private static class q {
-            private final String a;
-            private q(String a) { this.a = a; }
-            private String a() { return a; }
-        }
+        private JsonObject a() {
+            JsonObject f = new JsonObject();
+            f.addProperty("title", a);
+            f.addProperty("description", b);
+            f.addProperty("url", c);
 
-        private static class o {
-            private final String a;
-            private o(String a) { this.a = a; }
-            private String a() { return a; }
-        }
+            if (d != -1) f.addProperty("color", d);
 
-        private static class s {
-            private final String a, b, c;
-            private s(String a, String b, String c) { this.a = a; this.b = b; this.c = c; }
-            private String a() { return a; }
-            private String b() { return b; }
-            private String c() { return c; }
-        }
+            if (!e.isEmpty()) {
+                JsonArray g = new JsonArray();
+                for (c h : e) {
+                    g.add(h.a());
+                }
+                f.add("fields", g);
+            }
 
-        private static class u {
-            private final String a, b;
-            private final boolean c;
-            private u(String a, String b, boolean c) { this.a = a; this.b = b; this.c = c; }
-            private String a() { return a; }
-            private String b() { return b; }
-            private boolean c() { return c; }
+            return f;
+        }
+    }
+
+    private record c(String a, String b, boolean c) {
+        JsonObject a() {
+            JsonObject d = new JsonObject();
+            d.addProperty("name", a);
+            d.addProperty("value", b);
+            d.addProperty("inline", c);
+            return d;
         }
     }
 }
