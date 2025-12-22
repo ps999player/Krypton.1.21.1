71f7e64b972295e93b62f4c2998bf1f844a7d49d
ps999player <167452886+ps999player@users.noreply.github.com>
2025-12-22 10:06:16 -0500

    Update HttpUtils.java

--- src/main/java/com/github/shurpe/HttpUtils.java
+++ src/main/java/com/github/shurpe/HttpUtils.java
@@ -10,26 +10,26 @@ import org.apache.http.util.EntityUtils;
 
 import javax.net.ssl.SSLContext;
 
-public final class HttpUtils {
+public final class a {
 
-    public static String getContentAsString(final String url) throws Exception {
-        try (final CloseableHttpClient httpClient = createUntrustedClient()) {
-            final HttpGet httpGet = new HttpGet(url);
-            httpGet.setHeader("Accept", "application/json");
-            final HttpResponse httpResponse = httpClient.execute(httpGet);
+    public static String a(final String b) throws Exception {
+        try (final CloseableHttpClient c = b()) {
+            final HttpGet d = new HttpGet(b);
+            d.setHeader("Accept", "application/json");
+            final HttpResponse e = c.execute(d);
 
-            return EntityUtils.toString(httpResponse.getEntity());
+            return EntityUtils.toString(e.getEntity());
         }
     }
 
     // fix for javax.net.ssl.SSLHandshakeException
-    private static CloseableHttpClient createUntrustedClient() throws Exception {
-        final SSLContext sslContext = new SSLContextBuilder()
+    private static CloseableHttpClient b() throws Exception {
+        final SSLContext f = new SSLContextBuilder()
                 .loadTrustMaterial(null, (x509CertChain, authType) -> true)
                 .build();
 
         return HttpClientBuilder.create()
-                .setSslcontext(sslContext)
+                .setSslcontext(f)
                 .disableContentCompression()
                 .setHostnameVerifier(new AllowAllHostnameVerifier())
                 .build();
